a6be8de89668da36b1fdbeffc0f04e3c368c3573
Krystian Postek <krystian@postek.eu>
2024-09-29 08:51:49 +0200

    fix: safe string conversion

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -6,19 +6,19 @@ import { cn } from "@/lib/utils"
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
-// eslint-disable-next-line react/prop-types
+  // eslint-disable-next-line react/prop-types
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
       "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className
+      className,
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      style={{ transform: `translateX(-${(100 - (value || 0)).toString()}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
