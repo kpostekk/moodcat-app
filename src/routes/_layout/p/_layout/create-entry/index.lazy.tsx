bb1b9950b0a63372e072c858581ce9f6b624a4c4
Krystian Postek <krystian@postek.eu>
2024-09-29 11:17:32 +0200

    fix: refresh input selector

--- src/routes/_layout/p/_layout/create-entry/index.lazy.tsx
+++ src/routes/_layout/p/_layout/create-entry/index.lazy.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button"
 import { createLazyFileRoute, Link } from "@tanstack/react-router"
+import * as Icons from "lucide-react"
 
 export const Route = createLazyFileRoute("/_layout/p/_layout/create-entry/")({
   component: Component,
@@ -7,13 +8,19 @@ export const Route = createLazyFileRoute("/_layout/p/_layout/create-entry/")({
 
 function Component() {
   return (
-    <div className="grid gap-2">
+    <div className="grid gap-2 my-4">
+      <h1 className="text-3xl font-bold">Create new entry into your journal</h1>
       <Link to="/p/create-entry/voice">
-        <Button className="h-72 w-full text-xl">Talk to me</Button>
+        <Button className="h-24 w-full text-xl">
+          <Icons.Mic />
+          Speak into journal
+          <small className="translate-x-1 translate-y-1">(preferred)</small>
+        </Button>
       </Link>
       <Link to="/p/create-entry/text">
-        <Button variant="secondary" className="h-24 w-full text-xl">
-          Write to me
+        <Button variant="outline" className="h-24 w-full text-xl">
+          <Icons.Pencil />
+          Write into journal
         </Button>
       </Link>
     </div>
