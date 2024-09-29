88ac9a89e3955b8e70335d7f62cd1b1364fda65f
Krystian Postek <krystian@postek.eu>
2024-09-29 11:03:36 +0200

    fix: home sizing

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -13,7 +13,7 @@ function Component() {
   return (
     <div className={scheme}>
       <div className="grid h-screen w-screen place-items-center bg-background">
-        <Card className="group relative w-1/3 max-w-md overflow-hidden">
+        <Card className="group relative w-2/3 md:w-1/3 max-w-md overflow-hidden">
           {/* <div className="absolute z-10 grid h-full w-full select-none place-content-center bg-background/90 opacity-0 backdrop-blur-sm duration-200 group-hover:opacity-100 group-hover:backdrop-blur-md">
             <div>🚧 under construction 🚧</div>
           </div> */}
